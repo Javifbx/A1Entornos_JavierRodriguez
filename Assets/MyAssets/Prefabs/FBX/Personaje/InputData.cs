ee13fed939addaabc045da140e5d2a5abe582969
Javifbx <72795434+Javifbx@users.noreply.github.com>
2021-03-16 20:09:47 +0100

    Scena Nueva con el Personaje

---
+++ Assets/MyAssets/Prefabs/FBX/Personaje/InputData.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System;
+
+[Serializable]
+public struct InputData {
+    // Basic Movement
+    public float hMovement;
+    public float vMovement;
+
+    //Mouse rotation
+    public float verticalMouse;
+    public float horizontalMouse;
+
+    // Extra momevement
+    public bool dash;
+    public bool jump;
+
+    public void getInput()
+    {
+        // Basic Movement
+        hMovement = Input.GetAxis("Horizontal");
+        vMovement = Input.GetAxis("Vertical");
+
+        // Mouse/Joystick rotation
+        verticalMouse = Input.GetAxis("Mouse Y");
+       horizontalMouse = Input.GetAxis("Mouse X");
+
+        //Extra movement
+        dash = Input.GetButton("Dash");
+        jump = Input.GetButtonDown("Jump");
+    }
+}
